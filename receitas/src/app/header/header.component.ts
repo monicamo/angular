a40f679abb4eb66aca22869015a0f1ec293a904f
Monica Monteiro <monicamo@gmail.com>
2021-11-21 12:02:50 -0300

    RouterLink e RouterLinkActive

--- receitas/src/app/header/header.component.ts
+++ receitas/src/app/header/header.component.ts
@@ -7,16 +7,16 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() featureSelected: EventEmitter<string> = new EventEmitter<string>();
+  // @Output() featureSelected: EventEmitter<string> = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
+  // onSelect(feature: string) {
+  //   this.featureSelected.emit(feature);
+  // }
 
 }
 
