6e5f27866c351844544aeaf2b04542f6a4fa5ccf
Monica Monteiro <monicamo@gmail.com>
2021-11-21 11:38:25 -0300

    Subjects

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { UserService } from './user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
 
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
+
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
       (data: boolean) => {
         this.userActivated = data;
       }
